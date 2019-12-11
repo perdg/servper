94c69c41cbb73ce9593f1a7c93cb2f8a5cd896d0
perdg <bybas2@yandex.ru>
2019-12-11 19:35:31 +0300

    Updates

---
+++ precache-manifest.008488f6f2ff039edce7ce91cad5ebc4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2250d083b5cfcb3512436ee40e401e2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a9c039ab444f395df591",
+    "url": "./static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "c1b669095c1f318c07da",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "a9c039ab444f395df591",
+    "url": "./static/js/2.8a20b280.chunk.js"
+  },
+  {
+    "revision": "c1b669095c1f318c07da",
+    "url": "./static/js/main.bf5d9960.chunk.js"
+  },
+  {
+    "revision": "b55fb5112e5cbf9f353f",
+    "url": "./static/js/runtime-main.058f5714.js"
+  },
+  {
+    "revision": "6f25cb2468db79df52ab876bb4bdf089",
+    "url": "./static/media/persik.6f25cb24.png"
+  }
+]);
